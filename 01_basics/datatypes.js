b93abe41d242e15e31db6a149e4295513eba5d78
Soumojit24 <soumojithazra121004@gmail.com>
2024-09-12 14:52:35 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,25 @@
+"use strict" ;  //treat all JS code as newer version
+
+// alert(3+3)     //  we are using nodejs , not browser
+
+let name = "Soumojit"
+let age = 18
+let isLoggedIn = false
+
+
+
+// number  => 2 to the power of 53
+// bigint
+// string= ""
+// boolean 
+// null => standlone value
+// undefined =>
+//  symbol => unique
+
+
+//object
+
+console.log(typeof age);
+
+
+
---
+++ 01_basics/variables.js
@@ -0,0 +1,16 @@
+const accountId = 123
+let accountEmail = "Soumojit@google.com"
+var accountPassword = "144"
+accountCity = "Kolkata"
+//accountId=2
+
+let accountState
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+/* let,var = change the state
+const = do not change the state
+prefer no to use var
+because of issue in block scope and functional scaope
+*/
+
